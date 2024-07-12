956fba54ffd2bad33ce5b455be7b0327ab30e376
Jeremy Calafato <jeremycalafato@gmail.com>
2024-07-12 12:33:04 +0200

    Navigation Bar Enhancements 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
     <Grid templateAreas={{ base: '"nav" "main"' }}>
-
       <GridItem area="nav" backgroundColor="#2d3748" color="White">
         <NavBar></NavBar>
       </GridItem>
 
-      <GridItem area="main" backgroundColor="#1e1e1e" color="White">Main</GridItem>
-      
+      <GridItem area="main" backgroundColor="#1e1e1e" color="White">
+        Main
+      </GridItem>
     </Grid>
   );
-
 }
 
 export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,31 +1,132 @@
-import logo from "../assets/Games-Store Logo.png";
-import settings from "../assets/Settings Icon.png";
-import profile from "../assets/Profile Icon.png";
+import FullLogo from "../assets/Games-Store Logo.png";
+import SmallLogo from "../assets/Games-Store Logo Small.png";
+import profile from "../assets/Profile Icon 2.png";
+import { BsChevronDown, BsSearch } from "react-icons/bs";
 
-import { HStack, Image, Input } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Center,
+  HStack,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 function NavBar() {
-  return (
-    <HStack justifyContent={"space-between"}>
-      <Image src={logo} width="200px" padding="10px"></Image>
-      <Input
-        size="md"
-        width="800px"
-        borderRadius="14px"
-        backgroundColor="white"
-        color="#6C6C6C"
-        fontWeight="semibold"
-        placeholder="Search Game"
-        _placeholder={{ color: "#6C6C6C", fontWeight: "smibold" }}
-      ></Input>
+  // Show Full Logo on Medium-Sized Screens and Above
+  const showLogo = useBreakpointValue({
+    md: FullLogo,
+    base: SmallLogo,
+  });
 
-      <div>
-        <HStack padding="10px">
-          <Image src={settings} boxSize="40px"></Image>
-          <Image src={profile} boxSize="50px"></Image>
+  // Show Genres Filter on Medium-Sized Screens and Above
+  const showGenresFilter = useBreakpointValue({
+    md: true,
+    base: false,
+  });
+
+  // Change the Curviture of the Search Bar According to the Size of the Screen
+  const curveSearchBar = useBreakpointValue({
+    md: "14px 0px 0px 14px",
+    base: "14px 14px 14px 14px",
+  });
+
+  // Show "Search Games" Placeholder on Medium-Sized Screens and Above, Else Show "Search" Placeholder
+  const showPlaceholder = useBreakpointValue({
+    md: "Search Games",
+    base: "Search",
+  });
+
+  return (
+    <Box px={4} paddingY="10px">
+
+      {/* Spacing and Positioning of All Elements within NavBar */}
+      <HStack 
+        justifyContent={"space-between"} 
+        maxWidth="80em" 
+        margin="auto"
+      >
+
+        {/* Logo */}
+        <Image
+          src={showLogo}
+          width={{ base: "80px", md: "200px" }}
+          padding="10px"
+        />
+
+        {/* Search Bar */}
+        <HStack spacing={0} flex="1" mx="5%">
+          <InputGroup>
+            <InputRightElement
+              children={<BsSearch color="black"></BsSearch>}
+            ></InputRightElement>
+            <Input
+              flex="1"
+              borderRadius={curveSearchBar}
+              backgroundColor="white"
+              color="#6C6C6C"
+              fontWeight="semibold"
+              placeholder={showPlaceholder}
+              _placeholder={{ color: "#6C6C6C", fontWeight: "semibold" }}
+            />
+          </InputGroup>
+
+          {/* Genres Filter */}
+          {showGenresFilter && (
+            <Menu>
+              <MenuButton
+                as={Button}
+                borderRadius="0px 14px 14px 0px"
+                backgroundColor="#EDF4FF"
+                color="#434141"
+                minW="1.5em%"
+              >
+                <HStack>
+                  <span>Genres</span>
+                  rightIcon=
+                  {
+                    <BsChevronDown
+                      style={{ marginTop: "2px" }}
+                      size="0.9em"
+                    ></BsChevronDown>
+                  }
+                </HStack>
+              </MenuButton>
+              <MenuList></MenuList>
+            </Menu>
+          )}
         </HStack>
-      </div>
-    </HStack>
+
+        {/* Profile Icon */}
+        <Menu>
+          <MenuButton as={Button} variant={"link"} cursor={"pointer"}>
+            <Avatar size={"md"} src={profile} />
+          </MenuButton>
+          <MenuList alignItems={"center"} color="black">
+            <br />
+            <Center>
+              <Avatar size={"2xl"} src={profile} />
+            </Center>
+            <br />
+            <Center>
+              <p>Username</p>
+            </Center>
+            <br />
+            <MenuDivider></MenuDivider>
+            <MenuItem>Logout</MenuItem>
+          </MenuList>
+        </Menu>
+      </HStack>
+    </Box>
   );
 }
 
